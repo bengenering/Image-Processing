ec3622e9f271fdb440b645c5030930429154e4cc
apurva444 <apurva.randeria@gmail.com>
2018-03-18 17:57:39 +0530

    new_change

--- apurva/Untitled4.m
+++ apurva/Untitled4.m
@@ -6,7 +6,22 @@ a=img;
 imshow(a)
 clear cam;
 
-webcam_gettingstarted
-b=img;
-imshow(b)
-clear cam;
+c=rgb2gray(a);
+imshow(c)
+
+originimage=c
+imagemask=rgb2hsv(originimage);
+[row column k]=size(imagemask);
+h=imagemask(:,:,1);
+s=imagemask(:,:,2);
+v=imagemask(:,:,3);
+for i=1:row
+    for j=1:column
+        if h(i,j)>0.1
+            h(i,j)=0;
+        else
+            h(i,j)=1;
+        end
+    end
+end
+figure,imshow(h);
