e74ee76888a807011e38c05a64ddd9f2a0f4207d
Ashutosh Desai <bengenering@gmail.com>
2018-03-19 05:07:35 -0700

    Final Detection

---
+++ Ashutosh/face_detection2.m
@@ -0,0 +1,25 @@
+clear all
+clc
+% Create a cascade detector object.
+faceDetector = vision.CascadeObjectDetector();
+
+vidWriter = VideoWriter('frames.avi');
+open(vidWriter);
+for index = 1:20
+    % Acquire frame for processing
+    img = snapshot(webcam);
+    
+    % Write frame to video
+    writeVideo(vidWriter, img);
+end
+close(vidWriter);
+clear cam
+
+% Read a video frame and run the face detector.
+videoFileReader = vision.VideoFileReader('frames.avi');
+videoFrame      = step(videoFileReader);
+bbox            = step(faceDetector, videoFrame);
+
+% Draw the returned bounding box around the detected face.
+videoFrame = insertShape(videoFrame, 'Rectangle', bbox);
+figure; imshow(videoFrame); title('Detected face');
